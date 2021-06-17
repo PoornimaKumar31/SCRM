942f59e577286e7a18c7905de611d54bad4722c6
PS99003709 <digendrasahu11899@hotmail.com>
2021-06-17 12:51:28 +0530

    All File

---
+++ Sample/Features/Google.feature
@@ -0,0 +1,9 @@
+﻿Feature: Google Test
+
+@mytag
+Scenario: Search hello on google
+	Given user launched chrome browser
+	And navigate to google website
+	When user search for hello
+	Then user will see result
+	And user closes the browser
---
+++ Sample/Steps/GoogleStepDefinitions.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using TechTalk.SpecFlow;
+
+namespace Sample.Steps
+{
+    [Binding]
+    //bhag 
+    public sealed class GoogleStepDefinitions
+    {
+        IWebDriver driver;
+        [Given(@"user launched chrome browser")]
+        public void GivenUserLaunchedChromeBrowser()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            
+        }
+
+        [Given(@"navigate to google website")]
+        public void GivenNavigateToGoogleWebsite()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com/");
+        }
+
+        [When(@"user search for hello")]
+        public void WhenUserSearchForHello()
+        {
+            
+            driver.FindElement(By.Name("q")).SendKeys("Hello");
+            driver.FindElement(By.Name("q")).Submit();
+
+
+        }
+
+        [Then(@"user will see result")]
+        public void ThenUserWillSeeResult()
+        {
+            string expected = "Hello - Google Search";
+            string actual = driver.Title;
+            Assert.AreEqual(actual, expected);
+        }
+
+        [Then(@"user closes the browser")]
+        public void ThenUserClosesTheBrowser()
+        {
+            driver.Close();
+        }
+
+
+    }
+}
