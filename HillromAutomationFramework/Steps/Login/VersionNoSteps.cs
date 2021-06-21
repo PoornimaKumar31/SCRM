cc7b5d436233bdc626fb9f93d3c4a080e6b875fd
Sandesh Prabhu <prabhus1@hill-rom.com>
2021-06-21 10:26:03 +0000

    commented Nunit screenshot attachments.

--- HillromAutomationFramework/Steps/Login/VersionNoSteps.cs
+++ HillromAutomationFramework/Steps/Login/VersionNoSteps.cs
@@ -13,11 +13,11 @@ namespace HillromAutomationFramework.Steps.Login
         public void ThenVerifyIsDisplayed(string ExpectedText)
         {
             Assert.IsTrue(PropertyClass.Driver.PageSource.Contains(ExpectedText));
-            //Hooks1.CaptureNow();
+            Hooks1.CaptureNow();
             //adding attachment parts
-            var attachmentFilePath = $"{TestContext.CurrentContext.TestDirectory}\\{TestContext.CurrentContext.Test.MethodName}.jpg";
+            /*var attachmentFilePath = $"{TestContext.CurrentContext.TestDirectory}\\{TestContext.CurrentContext.Test.MethodName}.jpg";
             ((ITakesScreenshot)(PropertyClass.Driver)).GetScreenshot().SaveAsFile(attachmentFilePath);
-            TestContext.AddTestAttachment(attachmentFilePath);
+            TestContext.AddTestAttachment(attachmentFilePath);*/
         }
     }
 }
