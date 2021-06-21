fd923d07641682edc73ad595b14f97d79aa6cf4a
Sandesh Prabhu <prabhus1@hill-rom.com>
2021-06-21 14:49:37 +0530

    added attachment for Nunit Testing

--- HillromAutomationFramework/Steps/Login/VersionNoSteps.cs
+++ HillromAutomationFramework/Steps/Login/VersionNoSteps.cs
@@ -1,6 +1,7 @@
 ﻿using HillromAutomationFramework.Coding.SupportingCode;
 using HillromAutomationFramework.Hooks;
 using NUnit.Framework;
+using OpenQA.Selenium;
 using TechTalk.SpecFlow;
 
 namespace HillromAutomationFramework.Steps.Login
@@ -11,8 +12,12 @@ namespace HillromAutomationFramework.Steps.Login
         [Then(@"verify ""(.*)"" is displayed")]
         public void ThenVerifyIsDisplayed(string ExpectedText)
         {
-            Assert.IsTrue(PropertyClass.Driver.PageSource.Contains(ExpectedText));
+            Assert.IsFalse(PropertyClass.Driver.PageSource.Contains(ExpectedText));
             Hooks1.CaptureNow();
+            //adding attachment parts
+            var attachmentFilePath = $"{TestContext.CurrentContext.TestDirectory}\\{TestContext.CurrentContext.Test.MethodName}.jpg";
+            ((ITakesScreenshot)(PropertyClass.Driver)).GetScreenshot().SaveAsFile(attachmentFilePath);
+            TestContext.AddTestAttachment(attachmentFilePath);
         }
     }
 }
