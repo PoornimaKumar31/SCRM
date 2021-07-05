0e9de7d74c51f765d5ef38f8af94c1c73b4ad6db
Sandesh Prabhu <prabhus1@hill-rom.com>
2021-07-05 16:15:07 +0530

    Completed CSM device details feature

--- HillromAutomationFramework/Coding/PageObjects/CSMDeviceDetailsPage.cs
+++ HillromAutomationFramework/Coding/PageObjects/CSMDeviceDetailsPage.cs
@@ -14,19 +14,59 @@ namespace HillromAutomationFramework.Coding.PageObjects
             PageFactory.InitElements(PropertyClass.Driver, this);
         }
 
-        public static class Locator
+        public static class Locators
         {
+            //Editing device details
+            public const string CSMDeviceEditButtonID = "csm-edit";
+            public const string RoomFieldID = "room";
+            public const string BedFieldID = "bed";
+            public const string SaveButtonID = "save-device-detail";
+            public const string CancelButtonID = "cancel-device-detail";
+            public const string RoomAndBedDetailsXpath = "//*[@id=\"device-main\"]/div[1]/div[3]/p[6]";
+
+            //Log files
             public const string LogsButtonXpath = "//*[@id=\"device-main\"]/div[2]/div/div/div[3]";
             public const string LogFilesID = "device-details";
         }
 
-        [FindsBy(How = How.XPath, Using = Locator.LogsButtonXpath)]
-        public IWebElement LogsButton { get; set; }
-
-        [FindsBy(How = How.Id, Using = Locator.LogFilesID)]
-        public IWebElement LogFiles { get; set; }
+        public static class ExpectedValues
+        {
+            public const string NewRoomValue = "New Room";
+            public const string NewBedValue = "New Bed";
+            public const string UpdateRoomValue = "Update Room";
+            public const string UpdateBedValue = "Update Bed";
+            public const string RoomAndBedNotSet = "(not set)";
+        }
 
         [FindsBy(How = How.Id, Using = "555566667777")]
         public IList<IWebElement> CSMDevices { get; set; }
+
+        //Device details related
+        [FindsBy(How = How.Id, Using = Locators.CSMDeviceEditButtonID)]
+        public IWebElement CSMDeviceEditButton { get; set; }
+
+        [FindsBy(How = How.Id, Using = Locators.RoomFieldID)]
+        public IWebElement RoomField { get; set; }
+
+        [FindsBy(How = How.Id, Using = Locators.BedFieldID)]
+        public IWebElement BedField { get; set; }
+
+        [FindsBy(How = How.Id, Using = Locators.SaveButtonID)]
+        public IWebElement SaveButton { get; set; }
+
+        [FindsBy(How = How.Id, Using = Locators.CancelButtonID)]
+        public IWebElement CancelButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = Locators.RoomAndBedDetailsXpath)]
+        public IWebElement RoomAndBedDetails { get; set; }
+
+        //Log files related
+        [FindsBy(How = How.XPath, Using = Locators.LogsButtonXpath)]
+        public IWebElement LogsButton { get; set; }
+
+        [FindsBy(How = How.Id, Using = Locators.LogFilesID)]
+        public IWebElement LogFiles { get; set; }
+
+        
     }
 }
--- HillromAutomationFramework/Steps/DeviceDetails/EditCSMDeviceDetailsSteps.cs
+++ HillromAutomationFramework/Steps/DeviceDetails/EditCSMDeviceDetailsSteps.cs
@@ -1,9 +1,12 @@
 ﻿using HillromAutomationFramework.Coding.PageObjects;
 using HillromAutomationFramework.Coding.SupportingCode;
+using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using TechTalk.SpecFlow;
 
 namespace HillromAutomationFramework.Steps.EditDeviceDetails
@@ -13,117 +16,178 @@ namespace HillromAutomationFramework.Steps.EditDeviceDetails
     {
         readonly ScenarioContext _scenarioContext;
         readonly LoginPage loginPage = new LoginPage();
-        
+        readonly MainPage mainPage = new MainPage();
+        readonly DeviceDetailsPage deviceDetailsPage = new DeviceDetailsPage();
+        readonly CSMDeviceDetailsPage cSMDeviceDetailsPage = new CSMDeviceDetailsPage();
+        string ExistingRoomAndBed;
+
 
         public EditCSMDeviceDetailsSteps(ScenarioContext scenarioContext)
         {
             _scenarioContext = scenarioContext;
         }
 
-        [Given(@"user has selected CSM device")]
+        [Given(@"user has selected CSM device with details not set")]
         public void GivenUserHasSelectedCSMDevice()
         {
             loginPage.SignIn("AdminWithoutRollupPage");
-            List<IWebElement> rows = PropertyClass.Driver.FindElements(By.Id("555566667777")).ToList<IWebElement>();
-            Console.WriteLine("Number OF Rows:" + rows.Count);
-
-            rows[0].Click();
+            SelectElement selectAssetType = new SelectElement(mainPage.AssetTypeDropDown);
+            selectAssetType.SelectByText(MainPage.ExpectedValues.CSMDeviceName);
+            //select the row according to the data
+            cSMDeviceDetailsPage.CSMDevices[0].Clicks();
         }
         
         [Given(@"user is on Device Details page")]
-        public void GivenUserIsOnDeviceDetailsPage()
+        public void GivenUserIsOncSMDeviceDetailsPage()
         {
-            _scenarioContext.Pending();
+            Assert.IsTrue(deviceDetailsPage.AssetLabel.GetElementVisibility());
+            Assert.IsTrue(deviceDetailsPage.AssetData.GetElementVisibility());
         }
         
         [When(@"user clicks Edit button")]
         public void WhenUserClicksEditButton()
         {
-            _scenarioContext.Pending();
+            Thread.Sleep(1000);
+            ExistingRoomAndBed = cSMDeviceDetailsPage.RoomAndBedDetails.Text;
+            cSMDeviceDetailsPage.CSMDeviceEditButton.Clicks();
         }
         
         [Then(@"Room and Bed can be entered")]
         public void ThenRoomAndBedCanBeEntered()
         {
-            _scenarioContext.Pending();
+            cSMDeviceDetailsPage.RoomField.EnterText(CSMDeviceDetailsPage.ExpectedValues.NewRoomValue);
+            cSMDeviceDetailsPage.BedField.EnterText(CSMDeviceDetailsPage.ExpectedValues.NewBedValue);
         }
         
         [Then(@"Saved")]
         public void ThenSaved()
         {
-            _scenarioContext.Pending();
+            cSMDeviceDetailsPage.SaveButton.Clicks();
         }
         
         [Then(@"Entered Room, Bed is displayed on Device page")]
         public void ThenEnteredRoomBedIsDisplayedOnDevicePage()
         {
-            _scenarioContext.Pending();
+            //Need to upgrade
+            Thread.Sleep(500);
+            string ActualRoomAndBedText = cSMDeviceDetailsPage.RoomAndBedDetails.Text;
+            string ExpectedRoomAndBedText = CSMDeviceDetailsPage.ExpectedValues.NewRoomValue + "/" + CSMDeviceDetailsPage.ExpectedValues.NewBedValue;
+            Assert.AreEqual(ExpectedRoomAndBedText,ActualRoomAndBedText);
         }
 
         [Given(@"user has selected CSM device with Room, Bed")]
         public void GivenUserHasSelectedCSMDeviceWithRoomBed()
         {
-            _scenarioContext.Pending();
+            loginPage.SignIn("AdminWithoutRollupPage");
+            SelectElement selectAssetType = new SelectElement(mainPage.AssetTypeDropDown);
+            selectAssetType.SelectByText(MainPage.ExpectedValues.CSMDeviceName);
+            cSMDeviceDetailsPage.CSMDevices[1].Clicks();
         }
 
         [Then(@"the existing Room , Bed is editable")]
         public void ThenTheExistingRoomBedIsEditable()
         {
-            _scenarioContext.Pending();
+            //Clearing the input fields and entering the values
+            cSMDeviceDetailsPage.RoomField.Clear();
+            cSMDeviceDetailsPage.RoomField.EnterText(CSMDeviceDetailsPage.ExpectedValues.NewRoomValue);
+            cSMDeviceDetailsPage.BedField.Clear();
+            cSMDeviceDetailsPage.BedField.EnterText(CSMDeviceDetailsPage.ExpectedValues.NewBedValue);
         }
 
         [Then(@"Updated Room, Bed is displayed on Device page")]
         public void ThenUpdatedRoomBedIsDisplayedOnDevicePage()
         {
-            _scenarioContext.Pending();
+            //Need to upgrade
+            Thread.Sleep(500);
+            string ActualRoomAndBedText = cSMDeviceDetailsPage.RoomAndBedDetails.Text;
+            string ExpectedRoomAndBedText = CSMDeviceDetailsPage.ExpectedValues.NewRoomValue + "/" + CSMDeviceDetailsPage.ExpectedValues.NewBedValue;
+            Assert.AreEqual(ExpectedRoomAndBedText, ActualRoomAndBedText);
+        }
+
+
+        [Given(@"user has selected CSM device with Room, Bed details")]
+        public void GivenUserHasSelectedNewCSMDeviceWithRoomBed()
+        {
+            loginPage.SignIn("AdminWithoutRollupPage");
+            SelectElement selectAssetType = new SelectElement(mainPage.AssetTypeDropDown);
+            selectAssetType.SelectByText(MainPage.ExpectedValues.CSMDeviceName);
+            cSMDeviceDetailsPage.CSMDevices[2].Clicks();
         }
 
         [Then(@"the existing Room , Bed can be removed")]
         public void ThenTheExistingRoomBedCanBeRemoved()
         {
-            _scenarioContext.Pending();
+            cSMDeviceDetailsPage.RoomField.Clear();
+            cSMDeviceDetailsPage.BedField.Clear();
         }
 
         [Then(@"Blank Room, Bed is displayed on Device page")]
         public void ThenBlankRoomBedIsDisplayedOnDevicePage()
         {
-            _scenarioContext.Pending();
+            //Need to upgrade
+            Thread.Sleep(1000);
+            string ActualRoomAndBedText = cSMDeviceDetailsPage.RoomAndBedDetails.Text;
+            string ExpectedRoomAnBedText = CSMDeviceDetailsPage.ExpectedValues.RoomAndBedNotSet;
+            Assert.AreEqual(ExpectedRoomAnBedText, ActualRoomAndBedText);
         }
 
         [When(@"Room, Bed is entered and cancelled")]
         public void WhenRoomBedIsEnteredAndCancelled()
         {
-            _scenarioContext.Pending();
+            cSMDeviceDetailsPage.RoomField.EnterText(CSMDeviceDetailsPage.ExpectedValues.UpdateRoomValue);
+            cSMDeviceDetailsPage.BedField.EnterText(CSMDeviceDetailsPage.ExpectedValues.UpdateBedValue);
+            //Clicking on cancel button
+            cSMDeviceDetailsPage.CancelButton.Click();
         }
 
         [Then(@"Entered Room, Bed is not displayed on Device page")]
         public void ThenEnteredRoomBedIsNotDisplayedOnDevicePage()
         {
-            _scenarioContext.Pending();
+            //Need to upgarde
+            Thread.Sleep(500);
+            string ActualRoomAndBed = cSMDeviceDetailsPage.RoomAndBedDetails.Text;
+            string EnteredRoomAndBed = CSMDeviceDetailsPage.ExpectedValues.UpdateRoomValue + "/" + CSMDeviceDetailsPage.ExpectedValues.UpdateBedValue;
+            Assert.AreNotEqual(EnteredRoomAndBed, ActualRoomAndBed);
         }
 
         [When(@"Room, Bed is updated and cancelled")]
         public void WhenRoomBedIsUpdatedAndCancelled()
         {
-            _scenarioContext.Pending();
+            cSMDeviceDetailsPage.RoomField.Clear();
+            cSMDeviceDetailsPage.RoomField.EnterText(CSMDeviceDetailsPage.ExpectedValues.UpdateRoomValue);
+            cSMDeviceDetailsPage.BedField.Clear();
+            cSMDeviceDetailsPage.BedField.EnterText(CSMDeviceDetailsPage.ExpectedValues.UpdateBedValue);
+
+            //cancelling
+            cSMDeviceDetailsPage.CancelButton.Clicks();
         }
 
         [Then(@"Updated Room, Bed is not displayed on Device page")]
         public void ThenUpdatedRoomBedIsNotDisplayedOnDevicePage()
         {
-            _scenarioContext.Pending();
+            //Need to upgrade
+            Thread.Sleep(500);
+            string ActualRoomAndBed = cSMDeviceDetailsPage.RoomAndBedDetails.Text;
+            string UpdatedRoomAndBed = CSMDeviceDetailsPage.ExpectedValues.UpdateRoomValue + "/" + CSMDeviceDetailsPage.ExpectedValues.UpdateBedValue;
+            Assert.AreNotEqual(UpdatedRoomAndBed, ActualRoomAndBed);
         }
 
         [When(@"Room, Bed is removed and cancelled")]
         public void WhenRoomBedIsRemovedAndCancelled()
         {
-            _scenarioContext.Pending();
+            cSMDeviceDetailsPage.RoomField.Clear();
+            cSMDeviceDetailsPage.BedField.Clear();
+            //Cancelling
+            cSMDeviceDetailsPage.CancelButton.Clicks();
         }
 
         [Then(@"Room, Bed is not removed on Device page")]
         public void ThenRoomBedIsNotRemovedOnDevicePage()
         {
-            _scenarioContext.Pending();
+            //Need To Upgrade
+            Thread.Sleep(500);
+            string ActualRoomAndBed = cSMDeviceDetailsPage.RoomAndBedDetails.Text;
+            Assert.AreEqual(ExistingRoomAndBed, ActualRoomAndBed);
         }
 
 
