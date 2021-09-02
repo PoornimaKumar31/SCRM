d1a8f0cfb455eeb587c691e61563bda93dfe31f0
Sandesh Prabhu <prabhus1@hill-rom.com>
2021-09-02 21:46:27 +0530

    Fixed centrella firmware status last deployed search

--- HillromAutomationFramework/PageObjects/ReportsTab/FirmwareStatusPage.cs
+++ HillromAutomationFramework/PageObjects/ReportsTab/FirmwareStatusPage.cs
@@ -110,7 +110,7 @@ namespace HillromAutomationFramework.PageObjects
             public const string CentrellaFirmwareVersionSearchText = "1.33.006";
             public const string CentrellaStatusSearchText = "Started";
             public const string CentrellaLocationSearchText = "Batesville";
-            public const string CentrellaLastDeployedSearchText = "1.35.612";
+            public const string CentrellaLastDeployedSearchText = "1.36.000";
 
             //CSMSearch Text
             public const string CSMSerialNumberSearchText = "100010000000";
