1d2f4b8a3bba32bf2e49141dc6b1129251d447a4
Sandesh Prabhu <prabhus1@hill-rom.com>
2021-09-07 15:53:18 +0530

    Updated 5707

--- HillromAutomationFramework/Steps/UpdatesTab/UpgradeUpdate/Req5707Steps.cs
+++ HillromAutomationFramework/Steps/UpdatesTab/UpgradeUpdate/Req5707Steps.cs
@@ -1,4 +1,5 @@
-﻿using HillromAutomationFramework.PageObjects;
+﻿using FluentAssertions;
+using HillromAutomationFramework.PageObjects;
 using HillromAutomationFramework.SupportingCode;
 using NUnit.Framework;
 using OpenQA.Selenium;
@@ -6,60 +7,67 @@ using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using TechTalk.SpecFlow;
+using ExplicitWait = SeleniumExtras.WaitHelpers.ExpectedConditions;
 
 namespace HillromAutomationFramework.Steps.UpdatesTab.UpgradeUpdate
 {
     [Binding,Scope(Tag = "SoftwareRequirementID_5707")]
     class Req5707Steps
     {
-        LoginPage loginPage = new LoginPage();
-        LandingPage landingPage = new LandingPage();
-        MainPage mainPage = new MainPage();
-        UpdatesSelectUpdatePage updatesSelectUpdatePage = new UpdatesSelectUpdatePage();
+        private readonly LoginPage _loginPage;
+        private readonly LandingPage _landingPage;
+        private readonly MainPage _mainPage;
+        private readonly UpdatesSelectUpdatePage _updatesSelectUpdatePage;
 
-        WebDriverWait wait = new WebDriverWait(PropertyClass.Driver, TimeSpan.FromSeconds(10));
-        private ScenarioContext _scenarioContext;
+        private readonly ScenarioContext _scenarioContext;
+        private readonly WebDriverWait _wait;
 
         public Req5707Steps(ScenarioContext scenarioContext)
         {
             _scenarioContext = scenarioContext;
+            _wait = new WebDriverWait(PropertyClass.Driver, TimeSpan.FromSeconds(10));
+
+            _loginPage = new LoginPage();
+            _landingPage = new LandingPage();
+            _mainPage = new MainPage();
+            _updatesSelectUpdatePage = new UpdatesSelectUpdatePage();  
         }
         
 
         [Given(@"user is on Select Updates page")]
         public void GivenUserIsOnSelectUpdatesPage()
         {
-            loginPage.LogIn(LoginPage.LogInType.AdminWithRollUpPage);
-            landingPage.LNTAutomatedTestEastOrganizationFacilityPanelTest4Title.Click();
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.Id(MainPage.Locators.DeviceListTableID)));
-            mainPage.UpdatesTab.JavaSciptClick();
+            _loginPage.LogIn(LoginPage.LogInType.AdminWithRollUpPage);
+            _landingPage.LNTAutomatedTestEastOrganizationFacilityPanelTest4Title.Click();
+            _wait.Until(ExplicitWait.ElementExists(By.Id(MainPage.Locators.DeviceListTableID)));
+            _mainPage.UpdatesTab.JavaSciptClick();
         }
 
         [Given(@"CSM Asset type is selected")]
         public void GivenCSMAssetTypeIsSelected()
         {
-            updatesSelectUpdatePage.AssetTypeDropDown.SelectDDL(UpdatesSelectUpdatePage.ExpectedValues.CSMDeviceName);
-            string SelectedAssetType = updatesSelectUpdatePage.AssetTypeDropDown.GetSelectedOptionFromDDL();
+            _updatesSelectUpdatePage.AssetTypeDropDown.SelectDDL(UpdatesSelectUpdatePage.ExpectedValues.CSMDeviceName);
+            string SelectedAssetType = _updatesSelectUpdatePage.AssetTypeDropDown.GetSelectedOptionFromDDL();
             string ExpectedAssetType = UpdatesSelectUpdatePage.ExpectedValues.CSMDeviceName;
-            Assert.AreEqual(ExpectedAssetType, SelectedAssetType, "CSM is not selected as asset type.\n");
+            (SelectedAssetType).Should().BeEquivalentTo(ExpectedAssetType, because:"CSM Asset type should be selected when User CSM as Asset type");
         }
 
         [Given(@"Upgrade Update type is selected")]
         public void GivenUpgradeUpdateTypeIsSelected()
         {
-            updatesSelectUpdatePage.UpgradeTypeDropDown.SelectDDL(UpdatesSelectUpdatePage.ExpectedValues.UpdateTypeUpgrade);
+            _updatesSelectUpdatePage.UpgradeTypeDropDown.SelectDDL(UpdatesSelectUpdatePage.ExpectedValues.UpdateTypeUpgrade);
         }
 
         [When(@"user clicks Manage Active Updates button")]
         public void WhenUserClicksManageActiveUpdatesButton()
         {
-            updatesSelectUpdatePage.ManageActiveUpgradesButton.Click();
+            _updatesSelectUpdatePage.ManageActiveUpgradesButton.Click();
         }
 
-        [Then(@"Manage Upgrades page is displayed")]
+        [Then(@"Manage Upgrades page is displayed"), Then(@"Manage Active Updates page is displayed")]
         public void ThenManageUpgradesPageIsDisplayed()
         {
-            Assert.AreEqual(true, updatesSelectUpdatePage.ManageUpgradeLabel.GetElementVisibility(), "Manage Upgrades page is not displayed.");
+            (_updatesSelectUpdatePage.ManageUpgradeLabel.GetElementVisibility()).Should().BeTrue(because: "Manage Upgrades page should be displayed when user clicks Manage Active Updates button in CSM Upgrades page");
         }
 
         [Given(@"user is on Manage Upgrades page")]
@@ -75,82 +83,77 @@ namespace HillromAutomationFramework.Steps.UpdatesTab.UpgradeUpdate
         [Then(@"Destinations label is displayed")]
         public void ThenDestinationsLabelIsDisplayed()
         {
-            Assert.AreEqual(true, updatesSelectUpdatePage.DestinationLabel.GetElementVisibility(), "Destination label is not displayed");
-            Assert.AreEqual(UpdatesSelectUpdatePage.ExpectedValues.ManageUpgradesDestinationLabel, updatesSelectUpdatePage.DestinationLabel.Text, "Destination label is not matching with the expected value.");
+            (_updatesSelectUpdatePage.DestinationLabel.GetElementVisibility()).Should().BeTrue(because: "Destination label should be displayed in CSM Manage Active Upgrades page");
+            string DestinationLabelText = _updatesSelectUpdatePage.DestinationLabel.Text;
+            (DestinationLabelText).Should().BeEquivalentTo(UpdatesSelectUpdatePage.ExpectedValues.ManageUpgradesDestinationLabel, because: "Destination label should match with the expected value in CSM Manage Active Upgrades page");
         }
 
         [Then(@"location hierarchy selectors are displayed")]
         public void ThenLocationHierarchySelectorsAreDisplayed()
         {
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.Id(UpdatesSelectUpdatePage.Locators.ManageUpgradesFirstDeviceID)));
-            Assert.AreEqual(true, updatesSelectUpdatePage.LocationHeiearchySelector.GetElementVisibility(), "Location Heirarchy label is not displayed");
+            _wait.Until(ExplicitWait.ElementExists(By.Id(UpdatesSelectUpdatePage.Locators.ManageUpgradesFirstDeviceID)));
+            (_updatesSelectUpdatePage.LocationHeiearchySelector.GetElementVisibility()).Should().BeTrue(because: "Location Heirarchy should be displayed in CSM Manage Active Upgrades page");
         }
 
         [Then(@"count of selected devices is displayed")]
         public void ThenCountOfSelectedDevicesIsDisplayed()
         {
-            Assert.AreEqual(true, updatesSelectUpdatePage.CountOfSelectedDevice.GetElementVisibility(), "Count of device is not displayed");
+            (_updatesSelectUpdatePage.CountOfSelectedDevice.GetElementVisibility()).Should().BeTrue(because: "Count of selected device should be displayed in CSM Manage Active Upgrades page");
         }
 
         [Then(@"Page x of y label is displayed")]
         public void ThenPageXOfYLabelIsDisplayed()
         {
-            Assert.AreEqual(true, updatesSelectUpdatePage.PaginationXofY.GetElementVisibility(), "Pagination label is not displayed");
+            (_updatesSelectUpdatePage.PaginationXofY.GetElementVisibility()).Should().BeTrue(because: "Page x of y label should be displayed in CSM Manage Active Upgrades page");
         }
 
         [Then(@"Displaying x to y of z results label is displayed")]
         public void ThenDisplayingXToYOfZResultsLabelIsDisplayed()
         {
-            Assert.AreEqual(true, updatesSelectUpdatePage.PaginationDisplayXY.GetElementVisibility(), "Result label is not displayed");
+            (_updatesSelectUpdatePage.PaginationDisplayXY.GetElementVisibility()).Should().BeTrue(because: "Result label should be displayed in CSM Manage Active Upgrades page");
         }
 
         [Then(@"Cancel Upgrade button is displayed")]
         public void ThenCancelUpgradeButtonIsDisplayed()
         {
-            Assert.AreEqual(true, updatesSelectUpdatePage.CancelUpgradeButton.GetElementVisibility(), "CancelUpgrade button is not displayed");
+            (_updatesSelectUpdatePage.CancelUpgradeButton.GetElementVisibility()).Should().BeTrue(because: "Cancel Upgrade button should be displayed in CSM Manage Active Upgrades page");
         }
 
         [Then(@"Cancel Upgrade button is disabled")]
         public void ThenCancelUpgradeButtonIsDisabled()
         {
-            Assert.AreEqual(false, updatesSelectUpdatePage.CancelUpgradeButton.Enabled,"Button is Enabled");
+            (_updatesSelectUpdatePage.CancelUpgradeButton.Enabled).Should().BeFalse(because: "Cancel upgrade button should be disabled since user didn't select any device in CSM Manage Active Upgrades page");
         }
 
         [Then(@"""(.*)"" column heading is displayed")]
         public void ThenColumnHeadingIsDisplayed(string HeadingName)
         {
             IWebElement HeadingElement = null;
-            string ExpectedHeadingText = null;
             switch (HeadingName.ToLower().Trim())
             { 
-            case "firmware":
-                    HeadingElement = updatesSelectUpdatePage.ManageUpgradesFirmwareHeading;
-                ExpectedHeadingText = UpdatesSelectUpdatePage.ExpectedValues.FirmwareHeadingText;
-                break;
-            case "new firmware":
-                    HeadingElement = updatesSelectUpdatePage.ManageUpgradeNewFirmwareHeading;
-                    ExpectedHeadingText = UpdatesSelectUpdatePage.ExpectedValues.NewFirmwareHeadingText;
+                case "firmware":
+                        HeadingElement = _updatesSelectUpdatePage.ManageUpgradesFirmwareHeading;
                     break;
-            case "schedule":
-                    HeadingElement = updatesSelectUpdatePage.ManageUpgradeScheduleHeading;
-                    ExpectedHeadingText = UpdatesSelectUpdatePage.ExpectedValues.ScheduleHeadingText;
-                    break;
-            case "serial number":
-                    HeadingElement = updatesSelectUpdatePage.ManageUpgradeSerialNumberHeading;
-                    ExpectedHeadingText = UpdatesSelectUpdatePage.ExpectedValues.SerialNumberHeadingText;
-                    break;
-            case "location":
-                    HeadingElement = updatesSelectUpdatePage.ManageUpgradeLocationHeading;
-                    ExpectedHeadingText = UpdatesSelectUpdatePage.ExpectedValues.LocationHeadingText;
-                    break;
+                case "new firmware":
+                        HeadingElement = _updatesSelectUpdatePage.ManageUpgradeNewFirmwareHeading;
+                        break;
+                case "schedule":
+                        HeadingElement = _updatesSelectUpdatePage.ManageUpgradeScheduleHeading;
+                        break;
+                case "serial number":
+                        HeadingElement = _updatesSelectUpdatePage.ManageUpgradeSerialNumberHeading;
+                        break;
+                case "location":
+                        HeadingElement = _updatesSelectUpdatePage.ManageUpgradeLocationHeading;
+                        break;
 
-            default:
-                    Assert.Fail(HeadingName + " is a invalid heading name");
-                    break;
-        }
-             Assert.AreEqual(true, HeadingElement.GetElementVisibility(), HeadingName + " is not displayed.");
-             string ActualHeadingText = HeadingElement.Text.ToLower();
-             Assert.AreEqual(ExpectedHeadingText.ToLower(), ActualHeadingText, HeadingName + " not matches with the expected value");
+                default:
+                        Assert.Fail(HeadingName + " is a invalid heading name");
+                        break;
+            }
+            (HeadingElement.GetElementVisibility()).Should().BeTrue(because: HeadingName + " column heading should be displayed in CSM Manage Active Upgrades page table");
+             string ActualHeadingText = HeadingElement.Text;
+            (ActualHeadingText).Should().BeEquivalentTo(HeadingName, because: HeadingName + " column heading should match with the expected value in CSM Manage Active Upgrades page table");
         }
 
 
@@ -158,15 +161,23 @@ namespace HillromAutomationFramework.Steps.UpdatesTab.UpgradeUpdate
         [Then(@"Select all checkbox is in column (.*)")]
         public void ThenSelectAllCheckboxIsInColumn(int columnNumber)
         {
-            string firstcolumnId = updatesSelectUpdatePage.ManageUpgradesTableHeading.FindElements(By.TagName("div"))[columnNumber - 1].FindElement(By.TagName("input")).GetAttribute("id");
-            Assert.AreEqual("selectall", firstcolumnId, "Select all checkbox is not in column " + columnNumber);
+            string firstcolumnId = _updatesSelectUpdatePage.ManageUpgradesTableHeading.FindElements(By.TagName("div"))[columnNumber - 1].FindElement(By.TagName("input")).GetAttribute("id");
+            (firstcolumnId).Should().BeEquivalentTo(UpdatesSelectUpdatePage.Locators.ManageUpgradesSelectAllCheckBoxID, because: "Select all checkbox should be in column number " + columnNumber+ " in CSM Manage Active Upgrades page table");
         }
 
         [Then(@"""(.*)"" label is in column (.*)")]
         public void ThenLabelIsInColumn(string columnHeading, int columnNumber)
         {
-            IList<IWebElement> columns = updatesSelectUpdatePage.ManageUpgradesTableHeading.FindElements(By.TagName("div"));
-            Assert.AreEqual(columnHeading.ToLower().Trim(), columns[columnNumber - 1].Text.ToLower(), columnHeading + " is not in " + columnNumber);
+            IList<IWebElement> columnsList = _updatesSelectUpdatePage.ManageUpgradesTableHeading.FindElements(By.TagName("div"));
+
+            List<string> columnListText = new List<string>();
+            foreach (IWebElement column in columnsList)
+            {
+                columnListText.Add(column.Text.ToLower());
+            }
+            //Making as zero based Indexing
+            columnNumber--;
+            columnListText.Should().HaveElementAt(columnNumber, columnHeading.ToLower(), because: columnHeading + " column heading should be in column number " + columnNumber+ " in CSM Manage Active Upgrades page table");
         }
 
 
@@ -174,32 +185,27 @@ namespace HillromAutomationFramework.Steps.UpdatesTab.UpgradeUpdate
         [Then(@"Select all checkbox is unchecked")]
         public void ThenSelectAllCheckboxIsUnchecked()
         {
-            Assert.AreEqual(false, updatesSelectUpdatePage.ManageUpgradesSelectAllCheckBox.Selected, "Select all checkbox in column 1 is not displayed");
+            (_updatesSelectUpdatePage.ManageUpgradesSelectAllCheckBox.Selected).Should().BeFalse(because: "Select all checkbox should be unchecked by default in CSM Manage Active Upgrades page");
         }
         [When(@"user selects device")]
         public void WhenUserSelectsDevice()
         {
-            updatesSelectUpdatePage.ManageUpgradesFirstDevice.Click();
+            _updatesSelectUpdatePage.ManageUpgradesFirstDevice.Click();
         }
 
         [When(@"clicks Cancel upgrade button")]
         public void WhenClicksCancelUpgradeButton()
         {
             SetMethods.ScrollToBottomofWebpage();
-            updatesSelectUpdatePage.CancelUpgradeButton.Click();
+            _updatesSelectUpdatePage.CancelUpgradeButton.Click();
         }
 
         [Then(@"Selected Updates have been cancelled message is displayed")]
         public void ThenSelectedUpdatesHaveBeenCancelledMessageIsDisplayed()
         {
-            Assert.AreEqual(true, updatesSelectUpdatePage.ManageUpgradesMessage.GetElementVisibility(), "Upgrade Cancelled message is not displayed.");
-            Assert.AreEqual(UpdatesSelectUpdatePage.ExpectedValues.ManageUpgradeCancelMessage, updatesSelectUpdatePage.ManageUpgradesMessage.Text, "Upgrade Cancelled message is not matching the expected value.");
-        }
-
-        [Then(@"Manage Active Updates page is displayed")]
-        public void ThenManageActiveUpdatesPageIsDisplayed()
-        {
-            ThenManageUpgradesPageIsDisplayed();
+            (_updatesSelectUpdatePage.ManageUpgradesMessage.GetElementVisibility()).Should().BeTrue(because: "Upgrade Cancelled message should be displayed when user clicks enabled cancel button in CSM Manage Active Upgrades page");
+            string UpdateCancelMessageText = _updatesSelectUpdatePage.ManageUpgradesMessage.Text;
+            (UpdateCancelMessageText).Should().BeEquivalentTo(UpdatesSelectUpdatePage.ExpectedValues.ManageUpgradeCancelMessage, because: "Upgrade Cancelled message should match with the expected value in CSM Manage Active Upgrades page");
         }
 
     }
